700a4bbee5ca09fa082dcec395083a88b8494d5a
Dorette <dorette.schutte86@gmail.com>
2023-11-09 08:50:39 +0200

    chap3

--- chapter3/chap3.2.sql
+++ chapter3/chap3.2.sql
@@ -1,2 +1,11 @@
 --1
 
+numeric(4,1)
+--It provides four digits in total with one digit after the decimal.
+
+--2
+varchar(50)
+--It will allow you to sort by firstname or lastname.
+
+--3
+--It will give you an error - invalid input syntax for type timestamp with time zone: "4//2017"
