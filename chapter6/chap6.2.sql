d081017c890af526d4383f14f4d61d53dc9d89e4
Dorette <dorette.schutte86@gmail.com>
2023-11-13 16:56:35 +0200

    chapter6

---
+++ chapter6/chap6.2.sql
@@ -0,0 +1,63 @@
+--1
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2000.geo_name
+FROM us_counties_2010 c2010 LEFT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2000.geo_name IS NULL;
+
+-- Counties that exist in 2010 data but not 2000 include five county equivalents
+-- in Alaska plus Broomfield County, Colorado.
+
+SELECT c2010.geo_name,
+       c2000.geo_name,
+       c2000.state_us_abbreviation
+FROM us_counties_2010 c2010 RIGHT JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+WHERE c2010.geo_name IS NULL;
+
+-- Counties that exist in 2000 data but not 2010 include three county
+-- equivalents in Alaska plus Clifton Forge city, Virginia,
+-- which gave up its independent city status in 2001
+
+--2
+
+-- Using median():
+
+SELECT median(round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS median_pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips;
+
+-- Using percentile_cont():
+
+SELECT percentile_cont(.5)
+       WITHIN GROUP (ORDER BY round( (CAST(c2010.p0010001 AS numeric(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 )) AS percentile_50th
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips;
+
+-- 3.2%
+
+--3
+
+SELECT c2010.geo_name,
+       c2010.state_us_abbreviation,
+       c2010.p0010001 AS pop_2010,
+       c2000.p0010001 AS pop_2000,
+       c2010.p0010001 - c2000.p0010001 AS raw_change,
+       round( (CAST(c2010.p0010001 AS DECIMAL(8,1)) - c2000.p0010001)
+           / c2000.p0010001 * 100, 1 ) AS pct_change
+FROM us_counties_2010 c2010 INNER JOIN us_counties_2000 c2000
+ON c2010.state_fips = c2000.state_fips
+   AND c2010.county_fips = c2000.county_fips
+ORDER BY pct_change ASC;
+
+-- St. Bernard Parish, La. It and other Louisiana parishes (the county
+-- equivalent name in Louisiana) experienced substantial population loss
+-- following Hurricane Katrina in 2005
